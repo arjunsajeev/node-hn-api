554af90f2a6b1fc8746932f4bd8579705f189247
Arjun Sajeev <arjunsajeev04@gmail.com>
2017-12-10 21:04:04 +0530

    Add babel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-const fetch = require('node-fetch');
-
+const fetch = require('isomorphic-fetch')
 const {
   getItemURL,
   getUserURL,
@@ -9,69 +8,68 @@ const {
   askStoriesURL,
   jobStoriesURL,
   showStoriesURL
-} = require('./urls');
-
-const defaultNumberOfStories = 10;
+} = require('./urls')
+const defaultNumberOfStories = 10
 
 function fetchJSON(URL) {
-	return fetch(URL).then(response => response.json());
+  return fetch(URL).then(response => response.json())
 }
 
 // Items
 
 function fetchItem(itemID) {
-	return fetchJSON(getItemURL(itemID));
+  return fetchJSON(getItemURL(itemID))
 }
 
 function fetchItems(itemIDs) {
-	return Promise.all(itemIDs.map(itemID => fetchItem(itemID)));
+  return Promise.all(itemIDs.map(itemID => fetchItem(itemID)))
 }
 
 // Stories
 
 function fetchStories(storiesURL, numberOfStories = defaultNumberOfStories) {
-	return fetchJSON(storiesURL).then(itemIDs =>
+  return fetchJSON(storiesURL).then(itemIDs =>
     fetchItems(itemIDs.slice(0, numberOfStories))
-  );
+  )
 }
 
 function fetchTopStories(numberOfStories) {
-	return fetchStories(topStoriesURL, numberOfStories);
+  return fetchStories(topStoriesURL, numberOfStories)
 }
 
 function fetchNewStories(numberOfStories) {
-	return fetchStories(newStoriesURL, numberOfStories);
+  return fetchStories(newStoriesURL, numberOfStories)
 }
 
 function fetchBestStories(numberOfStories) {
-	return fetchStories(bestStoriesURL, numberOfStories);
+  return fetchStories(bestStoriesURL, numberOfStories)
 }
 
 function fetchAskStories(numberOfStories) {
-	return fetchStories(askStoriesURL, numberOfStories);
+  return fetchStories(askStoriesURL, numberOfStories)
 }
 
 function fetchJobStories(numberOfStories) {
-	return fetchStories(jobStoriesURL, numberOfStories);
+  return fetchStories(jobStoriesURL, numberOfStories)
 }
 
 function fetchShowStories(numberOfStories) {
-	return fetchStories(showStoriesURL, numberOfStories);
+  return fetchStories(showStoriesURL, numberOfStories)
 }
 
 // Users
 
 function fetchUser(userId) {
-	return fetchJSON(getUserURL(userId));
+  return fetchJSON(getUserURL(userId))
 }
 
 module.exports = {
-	fetchItem,
-	fetchUser,
-	fetchTopStories,
-	fetchNewStories,
-	fetchBestStories,
-	fetchAskStories,
-	fetchJobStories,
-	fetchShowStories
-};
+  fetchItem,
+  fetchUser,
+  fetchTopStories,
+  fetchNewStories,
+  fetchBestStories,
+  fetchAskStories,
+  fetchJobStories,
+  fetchShowStories
+}
