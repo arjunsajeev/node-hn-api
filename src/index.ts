208747c562c3cc00b10aab48448271fcf053e045
Arjun Sajeev <arjunsajeev04@gmail.com>
2020-05-05 16:13:42 +0530

    Format

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import fetchData from 'isomorphic-fetch'
+import fetchData from 'isomorphic-fetch';
 const {
   getItemURL,
   getUserURL,
@@ -7,69 +7,61 @@ const {
   newStoriesURL,
   askStoriesURL,
   jobStoriesURL,
-  showStoriesURL
-} = require('./urls')
-const defaultNumberOfStories = 10
+  showStoriesURL,
+} = require('./urls');
+const defaultNumberOfStories = 10;
 
-function fetchJSON(URL: string){
-  return fetchData(URL).then((response: Response) => response.json())
+function fetchJSON(URL: string) {
+  return fetchData(URL).then((response: Response) => response.json());
 }
 
 // Items
 
 export function fetchItem(itemID: number) {
-  return fetchJSON(getItemURL(itemID))
+  return fetchJSON(getItemURL(itemID));
 }
 
 function fetchItems(itemIDs: number[]): Promise<any> {
-  return Promise.all(itemIDs.map(itemID => fetchItem(itemID)))
+  return Promise.all(itemIDs.map(itemID => fetchItem(itemID)));
 }
 
 // Stories
 
-export function fetchStories(storiesURL: string, numberOfStories = defaultNumberOfStories) {
+export function fetchStories(
+  storiesURL: string,
+  numberOfStories = defaultNumberOfStories
+) {
   return fetchJSON(storiesURL).then((itemIDs: number[]) =>
     fetchItems(itemIDs.slice(0, numberOfStories))
-  )
+  );
 }
 
 export function fetchTopStories(numberOfStories: number) {
-  return fetchStories(topStoriesURL, numberOfStories)
+  return fetchStories(topStoriesURL, numberOfStories);
 }
 
 export function fetchNewStories(numberOfStories: number) {
-  return fetchStories(newStoriesURL, numberOfStories)
+  return fetchStories(newStoriesURL, numberOfStories);
 }
 
 export function fetchBestStories(numberOfStories: number) {
-  return fetchStories(bestStoriesURL, numberOfStories)
+  return fetchStories(bestStoriesURL, numberOfStories);
 }
 
 export function fetchAskStories(numberOfStories: number) {
-  return fetchStories(askStoriesURL, numberOfStories)
+  return fetchStories(askStoriesURL, numberOfStories);
 }
 
 export function fetchJobStories(numberOfStories: number) {
-  return fetchStories(jobStoriesURL, numberOfStories)
+  return fetchStories(jobStoriesURL, numberOfStories);
 }
 
 export function fetchShowStories(numberOfStories: number) {
-  return fetchStories(showStoriesURL, numberOfStories)
+  return fetchStories(showStoriesURL, numberOfStories);
 }
 
 // Users
 
 export function fetchUser(userId: string) {
-  return fetchJSON(getUserURL(userId))
+  return fetchJSON(getUserURL(userId));
 }
-
-// module.exports = {
-//   fetchItem,
-//   fetchUser,
-//   fetchTopStories,
-//   fetchNewStories,
-//   fetchBestStories,
-//   fetchAskStories,
-//   fetchJobStories,
-//   fetchShowStories
-// }
